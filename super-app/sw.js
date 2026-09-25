3175965ab5d2ee1c7dc5a48cf1cf5f9e3a80f869
lefortdenis-pixel <lefort.denis@gmail.com>
2026-09-25 21:40:46 +0200

    Refresh PWA cache for field feature pass

--- super-app/sw.js
+++ super-app/sw.js
@@ -1,4 +1,4 @@
-const CACHE='traversee-v1-154-robustness-final';
+const CACHE='traversee-v1-155-field-features';
 const TILE_CACHE='traversee-map-tiles-v1';
 const ASSETS=['./','./index.html','./water-points.json','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png','./bivouac/index.html','./vendor/leaflet.css','./vendor/leaflet.js','./vendor/leaflet.sync.js'];
 
