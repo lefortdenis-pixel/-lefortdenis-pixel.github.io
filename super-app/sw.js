73bf230f78a3c7f75dc2f3ff61795109d9c920ff
lefortdenis-pixel <lefort.denis@gmail.com>
2026-09-25 17:04:39 +0200

    Refresh PWA cache after robustness fixes

--- super-app/sw.js
+++ super-app/sw.js
@@ -1,4 +1,4 @@
-const CACHE='traversee-v1-153-robustness';
+const CACHE='traversee-v1-154-robustness-final';
 const TILE_CACHE='traversee-map-tiles-v1';
 const ASSETS=['./','./index.html','./water-points.json','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png','./bivouac/index.html','./vendor/leaflet.css','./vendor/leaflet.js','./vendor/leaflet.sync.js'];
 
