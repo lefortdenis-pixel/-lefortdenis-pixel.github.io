f93f9ef1bec62f38fe52203ef29b14b5cacb7464
lefortdenis-pixel <lefort.denis@gmail.com>
2026-09-25 08:12:46 +0200

    Refresh PWA cache for bivouac slider update

--- super-app/sw.js
+++ super-app/sw.js
@@ -1,4 +1,4 @@
-const CACHE='traversee-v1-140-daily-bivouac';
+const CACHE='traversee-v1-141-bivouac-slider';
 const ASSETS=['./','./index.html','./water-points.json','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png','./bivouac/index.html'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
@@ -6,6 +6,11 @@ self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET') return;
   const url=new URL(event.request.url);
   if(url.origin===self.location.origin){
-    event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return resp;}).catch(()=>caches.match('./index.html'))));
+    const htmlRequest=event.request.mode==='navigate'||url.pathname.endsWith('/index.html');
+    if(htmlRequest){
+      event.respondWith(fetch(event.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return resp;}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))));
+    }else{
+      event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return resp;})));
+    }
   }
 });
